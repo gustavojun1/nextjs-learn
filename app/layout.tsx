df8d86296415b8617c8fc4d0d9e6e50e23563f83
jun <gustavonagatomo@gmail.com>
2024-04-09 16:55:32 -0300

    Add CSS styles (with and without tailwind)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+// '@' is an alias (by default, means "./") defined in the tsconfig.json file
+// you can edit it or add new aliases
+import '@/app/ui/global.css'
+
 export default function RootLayout({
   children,
 }: {
