360500ee16974190b5de907bece711d030f4b634
jun <gustavonagatomo@gmail.com>
2024-04-10 18:04:28 -0300

    Add images

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import { lusitana } from '@/app/ui/fonts.ts'
+import { lusitana } from '@/app/ui/fonts'
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -30,6 +31,26 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          {/*the width and height should follow the aspect ratio of the image*/}
+          {/*the classes makes sure that the image is shown only on desktop screens*/}
+          {/*hidden -> hides image*/}
+          {/*block -> renders image as block-level element*/}
+          {/*md -> medium-sized screen breakpoint; md:x means that x will be applied to medium-sized screens AND LARGER*/}
+          {/*comments like this cannot be placed INSIDE a component lol (between "<>")*/}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width="560"
+            height="620"
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
